71d3311593644643e56fff30d40581b73a22919d
Joao Souza <jsouza@c1r5s9.42lisboa.com>
2026-03-04 17:16:40 +0000

    ex1 done

--- ex1/loading.py
+++ ex1/loading.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     fail = False
     try:
         import pandas
+        # prints the name and version of the package
         print(f"[OK] {pandas.__name__} ({pandas.__version__}) "
               "- Data manipulation ready")
     except ImportError:
@@ -18,6 +19,7 @@ if __name__ == "__main__":
         fail = True
     try:
         import requests
+        # prints the name and version of the package
         print(f"[OK] {requests.__name__} ({requests.__version__}) "
               "- Network access ready")
     except ImportError:
@@ -25,6 +27,7 @@ if __name__ == "__main__":
         fail = True
     try:
         import matplotlib
+        # prints the name and version of the package
         print(f"[OK] {matplotlib.__name__} ({matplotlib.__version__}) "
               "- Data Visualization ready")
     except ImportError:
@@ -36,21 +39,27 @@ if __name__ == "__main__":
 
     print("\nAnalyzing Matrix data...")
     try:
-        response = requests.get("https://api.worldbank.org/v2/country\
-/WLD/indicator/SP.POP.TOTL?format=json&per_page=20000&date=1980:2024")
-
-        if response.status_code != 200:
-            print(f"Connction failed: (status code={response.status_code})")
+        # attempts to access the World Bank API
+        try:
+            response = requests.get("https://api.worldbank.org/v2/country\
+/WLD/indicator/SP.POP.TOTL?format=json&per_page=20000&date=1960:2024")
+        except Exception as e:
+            print('\nConnection error:\n')
+            print(e)
             sys.exit(1)
+        
+        # take what you got from the API and format it
         data = response.json()[1]
         data_format = [{'year': item.get('date'),
                         'population': item.get('value')}
                        for item in data]
-
-        print(f'Processing {len(data_format)} data points...')
         data_format = sorted(data_format, key=support)
+
+        # processes the data using pandas
+        print(f'Processing {len(data_format)} data points...')
         processed = pandas.DataFrame(data_format)
 
+        # create a graph
         print("Generating visualization...")
         processed.plot(kind='line',
                        title="matrix_analysis",
@@ -59,6 +68,7 @@ if __name__ == "__main__":
                        x='year'
                        )
 
+        # save the graph
         matplotlib.pyplot.savefig("matrix_analysis.png")
     except Exception as e:
         print(e)
